a60f3fe43608924816ebe38c4e2f5f4d6a3b6d26
gkskzh100 <a22569447@gmail.com>
2016-08-01 17:27:36 +0900

    Kim ver 0.0.4
    많은걸 추가했다..

--- app/src/main/java/jm/dodam/newaragraphy/WriteActivity.java
+++ app/src/main/java/jm/dodam/newaragraphy/WriteActivity.java
@@ -1,14 +1,74 @@
 package jm.dodam.newaragraphy;
 
 import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
 
-public class WriteActivity extends Activity {
+public class WriteActivity extends AppCompatActivity {
+
+    private ImageButton writeChangeImageBtn;
+    private ImageButton writeAddTextBtn;
+    private ImageButton writeUploadBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        init();
+        setCustomActionbar();
+        setHideStatusBar();
+
+
+    }
+
+    private void init() {
+        writeChangeImageBtn = (ImageButton) findViewById(R.id.writeChangeImageBtn);
+        writeAddTextBtn = (ImageButton) findViewById(R.id.writeAddTextBtn);
+        writeUploadBtn = (ImageButton) findViewById(R.id.writeUploadBtn);
+    }
+
+    private void setListener() {
+        writeAddTextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+    }
+
+    private void setCustomActionbar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        actionBar.setDisplayShowCustomEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowTitleEnabled(false);
+
+        View mCustomView = LayoutInflater.from(this).inflate(R.layout.layout_actionbar,null);
+        actionBar.setCustomView(mCustomView);
+
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+
+
+
+        ActionBar.LayoutParams params = new ActionBar.LayoutParams(ActionBar.LayoutParams.MATCH_PARENT);
+        actionBar.setCustomView(mCustomView,params);
+    }
+
+    private void setHideStatusBar(){
+        if (Build.VERSION.SDK_INT >= 19) {      //Kitkat 이상 상태바 투명하게 만들기 //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT)
+            Window window = getWindow();
+            window.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
