bc9a32f983cefa0182ca49073be0af6c905f54c4
bhy98528 <bhy98528@gmail.com>
2016-08-02 10:28:12 +0900

    Bong ver 0.0.7

---
+++ app/src/main/java/jm/dodam/newaragraphy/SelectBackActivity.java
@@ -0,0 +1,15 @@
+package jm.dodam.newaragraphy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Bong on 201d6-08-02.
+ */
+public class SelectBackActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bg);
+    }
+}
---
+++ app/src/main/java/jm/dodam/newaragraphy/ShareActivity.java
@@ -0,0 +1,17 @@
+package jm.dodam.newaragraphy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by Bong on 2016-08-02.
+ */
+public class ShareActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+    }
+
+}
