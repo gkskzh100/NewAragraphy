1e2d94926462baedc9763929bf22ad52eb40c0b2
bhy98528 <bhy98528@gmail.com>
2016-08-05 07:04:15 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    
    #	app/src/main/java/jm/dodam/newaragraphy/controller/adapters/BackgroundAdapter.java

--- app/src/main/java/jm/dodam/newaragraphy/controller/adapters/BackgroundAdapter.java
+++ app/src/main/java/jm/dodam/newaragraphy/controller/adapters/BackgroundAdapter.java
@@ -1,22 +1,18 @@
-package jm.dodam.newaragraphy;
+package jm.dodam.newaragraphy.controller.adapters;
 
 import android.content.Context;
-import android.content.Intent;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
+
+import jm.dodam.newaragraphy.R;
 
 /**
  * Created by Bong on 2016-08-02.
@@ -24,13 +20,15 @@ import java.util.List;
 public class BackgroundAdapter extends RecyclerView.Adapter<BackgroundAdapter.ViewHolder> {
     ArrayList<String> images = null;
     Context context = null;
+
     public BackgroundAdapter(ArrayList<String> images, Context context) {
         this.images = images;
         this.context = context;
     }
+
     @Override
     public BackgroundAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_select_bg,parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_select_bg, parent, false);
         return new ViewHolder(view);
     }
 
@@ -51,11 +49,12 @@ public class BackgroundAdapter extends RecyclerView.Adapter<BackgroundAdapter.Vi
         return images.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
         public ImageView bgImage;
+
         public ViewHolder(View itemView) {
             super(itemView);
-            bgImage = (ImageView)itemView.findViewById(R.id.bgImage);
+            bgImage = (ImageView) itemView.findViewById(R.id.bgImage);
 
 
         }
